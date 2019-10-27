4d95e508226cd8d39eb6efacadbaf17cff724b2f
heilashaa <heilash_a_a@mail.ru>
2019-10-27 12:44:24 +0300

    start mustache

--- src/main/java/by/haapp/sweater/GreetingController.java
+++ src/main/java/by/haapp/sweater/GreetingController.java
@@ -1,22 +1,22 @@
 package by.haapp.sweater;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
-                    Model model) {
-        model.addAttribute("name", name);
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
         return "greeting";
     }
-
-
 
 
 
